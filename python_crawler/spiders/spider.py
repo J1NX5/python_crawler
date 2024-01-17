743136bb66a21d4c8912a128c3f84781c55c5a42
j1nx5 <j1nx@posteo.de>
2024-01-17 22:16:08 +0100

    Basic structure

---
+++ python_crawler/spiders/spider.py
@@ -0,0 +1,17 @@
+import scrapy
+from scrapy_selenium import SeleniumRequest
+
+class MySpider(scrapy.Spider):
+    name = 'my_spider'
+
+    def start_requests(self):
+        # Hier kannst du die URL deiner Zielwebsite angeben
+        url = 'https://www.tagesschau.de/'
+        
+        # SeleniumRequest wird verwendet, um JavaScript auf der Seite auszuführen
+        yield SeleniumRequest(url=url, callback=self.parse)
+
+    def parse(self, response):
+        # Hier kannst du auf den gerenderten HTML-Code zugreifen, der auch JavaScript enthält
+        # response.text enthält den HTML-Code nach der Ausführung von JavaScript
+        self.log(response.text)
